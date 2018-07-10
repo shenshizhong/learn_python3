9c5bc0a308c5046a5dff8013215fbd0993f5ec49
ssz <il>
2018-07-10 23:13:33 +0800

    操作文件和目录

---
+++ learn/two/IO编程/dir.py
@@ -0,0 +1,37 @@
+import os
+
+# 只要把下面的注释放开，每一个都可以单独运行。          （根据    操作文件和目录.md    的说明进行一步步操作即可）
+
+print(os.name)
+
+# print(os.environ)
+
+# print(os.environ.get('PATH'))
+
+# print(os.environ.get('x', 'default'))
+
+# print(os.path.abspath('.'))
+
+# print(os.path.join('E:\\pyplace\\learn_python3\\learn\\two\\IO编程', 'testdir'))
+
+# os.mkdir('E:\\pyplace\\learn_python3\\learn\\two\\IO编程\\testdir')
+
+# os.rmdir('E:\\pyplace\\learn_python3\\learn\\two\\IO编程\\testdir')
+
+# print(os.path.join('part1','part2'))
+
+# print(os.path.split('/users/michael/testdir/file.txt'))
+
+# print(os.path.splitext('/path/to/file.txt'))
+
+# os.rename('test.txt', 'test.py')
+
+# os.remove('test.py')
+
+# print([x for x in os.listdir('.') if os.path.isdir(x)])
+
+
+#print([x for x in os.listdir('.') if os.path.isfile(x)])
+
+# print([x for x in os.listdir('.') if os.path.isfile(x) and os.path.splitext(x)[1]=='.py'])
+
